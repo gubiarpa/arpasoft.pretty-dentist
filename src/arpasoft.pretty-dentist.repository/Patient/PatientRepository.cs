ce4bc8bdf0332f29bea9127f388360c791cf3d1a
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-05 19:08:35 -0500

    Create Repository Project

---
+++ src/arpasoft.pretty-dentist.repository/Patient/PatientRepository.cs
@@ -0,0 +1,8 @@
+﻿using arpasoft.pretty_dentist.abstractions.Repositories;
+
+namespace arpasoft.pretty_dentist.repository.Patient
+{
+    public class PatientRepository : IPatientRepository
+    {
+    }
+}
