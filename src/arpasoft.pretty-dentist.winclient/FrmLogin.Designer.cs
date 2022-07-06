58fd628d93307c17be7c8d8f8e53c255f76f77b9
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-06 17:09:01 -0500

    Styling FrmLogin

--- src/arpasoft.pretty-dentist.winclient/FrmLogin.Designer.cs
+++ src/arpasoft.pretty-dentist.winclient/FrmLogin.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtIUser = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIUser
+            // 
+            this.txtIUser.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.txtIUser.Location = new System.Drawing.Point(23, 52);
+            this.txtIUser.Name = "txtIUser";
+            this.txtIUser.Size = new System.Drawing.Size(177, 23);
+            this.txtIUser.TabIndex = 0;
+            // 
+            // lblUser
+            // 
+            this.lblUser.Location = new System.Drawing.Point(23, 26);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(100, 23);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Usuario";
+            this.lblUser.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox1.Location = new System.Drawing.Point(23, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Location = new System.Drawing.Point(23, 96);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(100, 23);
+            this.lblPassword.TabIndex = 0;
+            this.lblPassword.Text = "Contraseña";
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(23, 188);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(177, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Ingresar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(230, 247);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtIUser);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtIUser;
+        private Label lblUser;
+        private TextBox textBox1;
+        private Label lblPassword;
+        private Button btnLogin;
     }
 }
