433df655f939832a83e385a1470e3e0e8877215f
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-05 19:18:45 -0500

    Create Default Winform Project

---
+++ src/arpasoft.pretty-dentist.winclient/Form1.cs
@@ -0,0 +1,10 @@
+namespace arpasoft.pretty_dentist.winclient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
