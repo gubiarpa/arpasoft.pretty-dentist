1bdc60a57388609c5cdb6fb78a54f12229e8012e
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-30 15:19:52 -0500

    Patient Detail Tab - Part 2

--- src/arpasoft.pretty-dentist.winclient/Patient/FrmPatient.Designer.cs
+++ src/arpasoft.pretty-dentist.winclient/Patient/FrmPatient.Designer.cs
@@ -30,19 +30,19 @@
         {
             this.tabPatient = new System.Windows.Forms.TabControl();
             this.tabPatientList = new System.Windows.Forms.TabPage();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.datDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.txtTelephone = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblTelephone = new System.Windows.Forms.Label();
+            this.lblDateOfBirth = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtMaternalSurname = new System.Windows.Forms.TextBox();
+            this.lblMaternalSurname = new System.Windows.Forms.Label();
+            this.txtPaternalSurname = new System.Windows.Forms.TextBox();
+            this.lblPaternalSurname = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.lblDNI = new System.Windows.Forms.Label();
             this.tabPatientDetail = new System.Windows.Forms.TabPage();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.tabPatient.SuspendLayout();
             this.tabPatientList.SuspendLayout();
             this.SuspendLayout();
@@ -63,18 +63,18 @@
             // tabPatientList
             // 
             this.tabPatientList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tabPatientList.Controls.Add(this.dateTimePicker1);
-            this.tabPatientList.Controls.Add(this.textBox5);
-            this.tabPatientList.Controls.Add(this.textBox4);
-            this.tabPatientList.Controls.Add(this.label6);
-            this.tabPatientList.Controls.Add(this.label5);
-            this.tabPatientList.Controls.Add(this.label4);
-            this.tabPatientList.Controls.Add(this.textBox3);
-            this.tabPatientList.Controls.Add(this.label3);
-            this.tabPatientList.Controls.Add(this.textBox2);
-            this.tabPatientList.Controls.Add(this.label2);
-            this.tabPatientList.Controls.Add(this.textBox1);
-            this.tabPatientList.Controls.Add(this.label1);
+            this.tabPatientList.Controls.Add(this.datDateOfBirth);
+            this.tabPatientList.Controls.Add(this.txtTelephone);
+            this.tabPatientList.Controls.Add(this.txtName);
+            this.tabPatientList.Controls.Add(this.lblTelephone);
+            this.tabPatientList.Controls.Add(this.lblDateOfBirth);
+            this.tabPatientList.Controls.Add(this.lblName);
+            this.tabPatientList.Controls.Add(this.txtMaternalSurname);
+            this.tabPatientList.Controls.Add(this.lblMaternalSurname);
+            this.tabPatientList.Controls.Add(this.txtPaternalSurname);
+            this.tabPatientList.Controls.Add(this.lblPaternalSurname);
+            this.tabPatientList.Controls.Add(this.txtDNI);
+            this.tabPatientList.Controls.Add(this.lblDNI);
             this.tabPatientList.Location = new System.Drawing.Point(4, 34);
             this.tabPatientList.Name = "tabPatientList";
             this.tabPatientList.Padding = new System.Windows.Forms.Padding(3);
@@ -83,87 +83,103 @@
             this.tabPatientList.Text = "Detalle";
             this.tabPatientList.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
+            // datDateOfBirth
             // 
-            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(250, 233);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(163, 31);
-            this.dateTimePicker1.TabIndex = 4;
+            this.datDateOfBirth.CustomFormat = "dd/MM/yyyy";
+            this.datDateOfBirth.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datDateOfBirth.Location = new System.Drawing.Point(250, 233);
+            this.datDateOfBirth.Name = "datDateOfBirth";
+            this.datDateOfBirth.Size = new System.Drawing.Size(163, 31);
+            this.datDateOfBirth.TabIndex = 4;
             // 
-            // textBox4
+            // txtTelephone
             // 
-            this.textBox4.Location = new System.Drawing.Point(250, 183);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(402, 31);
-            this.textBox4.TabIndex = 3;
+            this.txtTelephone.Location = new System.Drawing.Point(250, 283);
+            this.txtTelephone.Name = "txtTelephone";
+            this.txtTelephone.Size = new System.Drawing.Size(163, 31);
+            this.txtTelephone.TabIndex = 5;
             // 
-            // label5
+            // txtName
             // 
-            this.label5.Location = new System.Drawing.Point(25, 233);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(219, 31);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Fecha Nacimiento";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txtName.Location = new System.Drawing.Point(250, 183);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(402, 31);
+            this.txtName.TabIndex = 3;
             // 
-            // label4
+            // lblTelephone
             // 
-            this.label4.Location = new System.Drawing.Point(25, 183);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(219, 31);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Nombres";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTelephone.Location = new System.Drawing.Point(25, 283);
+            this.lblTelephone.Name = "lblTelephone";
+            this.lblTelephone.Size = new System.Drawing.Size(219, 31);
+            this.lblTelephone.TabIndex = 0;
+            this.lblTelephone.Text = "Teléfono";
+            this.lblTelephone.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // textBox3
+            // lblDateOfBirth
             // 
-            this.textBox3.Location = new System.Drawing.Point(250, 133);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(402, 31);
-            this.textBox3.TabIndex = 2;
+            this.lblDateOfBirth.Location = new System.Drawing.Point(25, 233);
+            this.lblDateOfBirth.Name = "lblDateOfBirth";
+            this.lblDateOfBirth.Size = new System.Drawing.Size(219, 31);
+            this.lblDateOfBirth.TabIndex = 0;
+            this.lblDateOfBirth.Text = "Fecha Nacimiento";
+            this.lblDateOfBirth.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label3
+            // lblName
             // 
-            this.label3.Location = new System.Drawing.Point(25, 133);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(219, 31);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Apellido Materno";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblName.Location = new System.Drawing.Point(25, 183);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(219, 31);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nombres";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // textBox2
+            // txtMaternalSurname
             // 
-            this.textBox2.Location = new System.Drawing.Point(250, 83);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(402, 31);
-            this.textBox2.TabIndex = 1;
+            this.txtMaternalSurname.Location = new System.Drawing.Point(250, 133);
+            this.txtMaternalSurname.Name = "txtMaternalSurname";
+            this.txtMaternalSurname.Size = new System.Drawing.Size(402, 31);
+            this.txtMaternalSurname.TabIndex = 2;
             // 
-            // label2
+            // lblMaternalSurname
             // 
-            this.label2.Location = new System.Drawing.Point(25, 83);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(219, 31);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Apellido Paterno";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblMaternalSurname.Location = new System.Drawing.Point(25, 133);
+            this.lblMaternalSurname.Name = "lblMaternalSurname";
+            this.lblMaternalSurname.Size = new System.Drawing.Size(219, 31);
+            this.lblMaternalSurname.TabIndex = 0;
+            this.lblMaternalSurname.Text = "Apellido Materno";
+            this.lblMaternalSurname.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // textBox1
+            // txtPaternalSurname
             // 
-            this.textBox1.Location = new System.Drawing.Point(250, 33);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(402, 31);
-            this.textBox1.TabIndex = 0;
+            this.txtPaternalSurname.Location = new System.Drawing.Point(250, 83);
+            this.txtPaternalSurname.Name = "txtPaternalSurname";
+            this.txtPaternalSurname.Size = new System.Drawing.Size(402, 31);
+            this.txtPaternalSurname.TabIndex = 1;
             // 
-            // label1
+            // lblPaternalSurname
             // 
-            this.label1.Location = new System.Drawing.Point(25, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(219, 31);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "DNI";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblPaternalSurname.Location = new System.Drawing.Point(25, 83);
+            this.lblPaternalSurname.Name = "lblPaternalSurname";
+            this.lblPaternalSurname.Size = new System.Drawing.Size(219, 31);
+            this.lblPaternalSurname.TabIndex = 0;
+            this.lblPaternalSurname.Text = "Apellido Paterno";
+            this.lblPaternalSurname.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(250, 33);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(402, 31);
+            this.txtDNI.TabIndex = 0;
+            // 
+            // lblDNI
+            // 
+            this.lblDNI.Location = new System.Drawing.Point(25, 33);
+            this.lblDNI.Name = "lblDNI";
+            this.lblDNI.Size = new System.Drawing.Size(219, 31);
+            this.lblDNI.TabIndex = 0;
+            this.lblDNI.Text = "DNI";
+            this.lblDNI.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // tabPatientDetail
             // 
@@ -175,22 +191,6 @@
             this.tabPatientDetail.Text = "Lista";
             this.tabPatientDetail.UseVisualStyleBackColor = true;
             // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(250, 283);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(163, 31);
-            this.textBox5.TabIndex = 5;
-            // 
-            // label6
-            // 
-            this.label6.Location = new System.Drawing.Point(25, 283);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(219, 31);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Teléfono";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
             // FrmPatient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
@@ -199,7 +199,7 @@
             this.Controls.Add(this.tabPatient);
             this.Name = "FrmPatient";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FrmPatient";
+            this.Text = "Pacientes";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmPatient_FormClosed);
             this.Load += new System.EventHandler(this.FrmPatient_Load);
             this.tabPatient.ResumeLayout(false);
@@ -214,17 +214,17 @@
         private TabControl tabPatient;
         private TabPage tabPatientList;
         private TabPage tabPatientDetail;
-        private TextBox textBox3;
-        private Label label3;
-        private TextBox textBox2;
-        private Label label2;
-        private TextBox textBox1;
-        private Label label1;
-        private DateTimePicker dateTimePicker1;
-        private TextBox textBox4;
-        private Label label5;
-        private Label label4;
-        private TextBox textBox5;
-        private Label label6;
+        private TextBox txtMaternalSurname;
+        private Label lblMaternalSurname;
+        private TextBox txtPaternalSurname;
+        private Label lblPaternalSurname;
+        private TextBox txtDNI;
+        private Label lblDNI;
+        private DateTimePicker datDateOfBirth;
+        private TextBox txtName;
+        private Label lblDateOfBirth;
+        private Label lblName;
+        private TextBox txtTelephone;
+        private Label lblTelephone;
     }
 }
